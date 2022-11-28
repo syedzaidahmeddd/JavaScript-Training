99c6dda363c613a01a52238ba7522097823ab5bd
syedzaidahmeddd <syedzaidahmed812@gmail.com>
2022-11-28 23:44:51 +0530

    hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+// To Print Output
+console.log("helloworld !");
+console.log("Age:20");
+console.log("College:LIET")
+// below line is doing multiplication
+console.log(5*2)
+/* 
+hello im zaid
+my age is 20
+hahahaha
+*/
